a6960bb86ea43aaae71f5bad64bdac60accfb51e
agentiauniv <pfc370516@gmail.com>
2026-02-20 22:19:24 +0100

    Update index.php

--- index.php
+++ index.php
@@ -21,8 +21,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     ]);
 
     $response = curl_exec($ch);
-    echo $response;
-    exit();
+    
     curl_close($ch);
 
     $data = json_decode($response, true);
@@ -132,6 +131,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
 </body>
 </html>
+
 
 
 
