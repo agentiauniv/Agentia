55f1de2592020a85e23580dc0f9d0b2efd88522e
agentiauniv <pfc370516@gmail.com>
2026-02-20 13:58:51 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,13 +2,11 @@
 session_start();
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-
     $email = $_POST['email'] ?? '';
     $password = $_POST['password'] ?? '';
 
     $supabase_url = "https://uhqqzlpaybcyxrepisgi.supabase.co/rest/v1/login";
     $api_key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVocXF6bHBheWJjeXhyZXBpc2dpIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3MDg0MDI3OCwiZXhwIjoyMDg2NDE2Mjc4fQ.zgY2AsO71vrf5V1lWW0J35nUtut1qUvfvGTRAHFRz7Y
-
 ";
 
     $headers = [
@@ -32,9 +30,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         header("Location: dashboard.php");
         exit();
     } else {
-        header("Location: index.php");
-        exit();
+        $error = "Email ou mot de passe incorrect";
     }
 }
 ?>
-
